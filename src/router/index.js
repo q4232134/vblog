713e211862b51bb26579450540a4f5d3a9ef040f
huangxizhou <195313059@qq.com>
2021-05-12 17:17:38 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/',
+    name: 'Blog',
+    component: () => import('../views/Blog.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
